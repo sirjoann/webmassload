3676cf86db4e0c6725f7413c5c399f01b7791b8b
sirjoann <sirjoann@gmail.com>
2017-06-16 17:57:46 +0700

    new

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MASSLOAD No.1 Factory of Die-Cast aluminum CCTV housing and bracket</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- animate -->
+        <link href="css/animate.css" rel="stylesheet">
+
+        <!-- font awesome -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+        <!-- font thai -->
+        <link href="fonts/stylesheet.css" rel="stylesheet">
+
+        <!-- Google font -->
+        <link href="https://fonts.googleapis.com/css?family=Mitr:300|Open+Sans:400,300,700|Pacifico|Raleway&amp;subset=thai" rel="stylesheet">
+
+        <!-- Custom -->
+        <link  href="css/full-slider.css" rel="stylesheet">
+        <link  href="css/new-age.css" rel="stylesheet">
+        <link  href="css/style.css" rel="stylesheet">
+    </head>
